d1f6f395992f050c242cc8de48325e6724e05692
Damon Oehlman <damon.oehlman@sidelab.com>
2013-01-19 14:17:52 +1000

    Started adding tests, need to create more command mappings

---
+++ test/create-drop.js
@@ -0,0 +1,19 @@
+var assert = require('assert'),
+    uuid = require('node-uuid'),
+    config = require('./config'),
+    orienteer = require('../'),
+    testDbName = uuid.v4(),
+    connection;
+
+describe('simple connection tests', function() {
+    it('should be able to create an orienteer connection object', function() {
+        assert(connection = orienteer(config.connection));
+    });
+
+    it('should be able to create a test database', function(done) {
+        connection.createDb({ name: testDbName }, function(err) {
+            assert.ifError(err);
+            done();
+        });
+    });
+});
