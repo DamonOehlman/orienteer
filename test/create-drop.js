ed9ac15ea83a56c8fb72f0ed494af771cbc596a3
Damon Oehlman <damon.oehlman@sidelab.com>
2013-01-20 22:38:40 +1000

    Added dbExist test after drop

--- test/create-drop.js
+++ test/create-drop.js
@@ -27,4 +27,14 @@ describe('simple connection tests', function() {
     it('should be able to drop the test database', function(done) {
         connection.dbDelete({ name: testDbName }, done);
     });
+
+    it('should be able to verify the db does not exist', function(done) {
+        connection.dbExist({ name: testDbName }, function(err, response) {
+            assert.ifError(err);
+            assert(response);
+            assert(! response.exists);
+
+            done();
+        });
+    });
 });
