c809b88abd03817709ffb95cf10ee05fe0a74c9b
Damon Oehlman <damon.oehlman@sidelab.com>
2013-01-22 23:59:05 +1000

    Added configuration overrides

--- test/config/index.js
+++ test/config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     connection: {
-        host: 'localhost',
-        port: 2424,
+        host: process.env.ORIENTDB_HOST || 'localhost',
+        port: process.env.ORIENTDB_PORT || 2424,
         user: 'root',
         password: process.env.ORIENTDB_ROOTPASS || 'FDB6C4ED52608E5942F84FE0CAE4C33462846C0B2F0FE0AAE5FA492BB72B6DDE'
     }
