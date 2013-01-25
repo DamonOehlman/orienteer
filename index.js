c830e2282fb76bc061183eac8257a1bfcc1b0458
Damon Oehlman <damon.oehlman@sidelab.com>
2013-01-25 15:18:30 +1000

    Added objectTo functionality for generating sql SET statements

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var Connection = require('./lib/connection'),
     OrientError = require('./lib/errors').OrientError,
-    _ = require('underscore');
+    _ = require('underscore'),
+    converters = require('./lib/converters');
 
-module.exports = function(opts) {
+var orienteer = module.exports = function(opts) {
     return new Connection(opts);
 };
 
+// add the hashToSQLSets utility which is very helpful from node-orientdb
+orienteer.objectTo = function(target, data) {
+	var converter = converters['objectTo' + target];
+
+	if (typeof converter != 'function') {
+		throw new Error('Unable to convert object to ' + target);
+	}
+
+	return converter(data);
+};
---
+++ test/converter-sql-set.js
@@ -0,0 +1,32 @@
+var assert = require('assert'),
+    orienteer = require('../');
+
+describe('object to SQL set statment conversion', function() {
+    it('should quote string elements', function() {
+        var statement = orienteer.objectTo('SET', { name: 'Fred' });
+
+        assert(statement);
+        assert.equal(statement, 'SET name = "Fred"');
+    });
+
+    it('should not quote non-strings', function() {
+        var statement = orienteer.objectTo('SET', { age: 35 });
+
+        assert(statement);
+        assert.equal(statement, 'SET age = 35');
+    });
+
+    it('should convert an empty object to an empty string', function() {
+        assert(orienteer.objectTo('SET', {}) === '');
+    });
+
+    it('should convert a multi-value object to a comma-separated statement', function() {
+        var statement = orienteer.objectTo('SET', {
+                name: 'Fred',
+                age: 35            
+            });
+
+        assert(statement);
+        assert.equal(statement, 'SET name = "Fred", age = 35');
+    });
+});
