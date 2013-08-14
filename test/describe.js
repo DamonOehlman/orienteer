20f110733e06592c8945f0a38fb2aefeec5d2844
Damon Oehlman <damon.oehlman@gmail.com>
2013-08-14 20:57:08 +1000

    Updated describe tests for 1.5.x compatability

--- test/describe.js
+++ test/describe.js
@@ -1,43 +1,44 @@
-var assert = require('assert'),
-    uuid = require('node-uuid'),
-    errors = require('../lib/errors'),
-    config = require('./config'),
-    orienteer = require('../'),
-    testDbName = uuid.v4(),
-    connection = orienteer(config);
+var assert = require('assert');
+var uuid = require('uuid');
+var errors = require('../lib/errors');
+var config = require('./config');
+var orienteer = require('../');
+var testDbName = uuid.v4();
+var connection = orienteer(config);
 
 describe('describe tests', function() {
-    before(function(done) {
-        connection.as('root').dbCreate({ name: testDbName }, done);
+  before(function(done) {
+    connection.as('root').dbCreate({ name: testDbName }, done);
+  });
+
+  after(function(done) {
+    connection.as('root').dbDelete({ name: testDbName }, done);
+  });
+
+  it('should be able to describe the test database', function(done) {
+    connection.describe(testDbName, function(err, info) {
+      assert.ifError(err);
+
+      assert(info, 'No info received on db: ' + testDbName);
+      done();
     });
+  });
 
-    after(function(done) {
-        connection.as('root').dbDelete({ name: testDbName }, done);
+  it('should be able to describe the OIdentity class', function(done) {
+    connection.db(testDbName).describe('OIdentity', function(err, info) {
+      assert.ifError(err);
+
+      assert(info, 'No info received on OIdentity class');
+      assert.equal(info.name, 'OIdentity');
+
+      done();
     });
+  });
 
-    it('should be able to describe the test database', function(done) {
-        connection.describe(testDbName, function(err, info) {
-            assert.ifError(err);
-
-            assert(info, 'No info received on db: ' + testDbName);
-            done();
-        });
+  it('should be able return a no-class error when attempting to describe a non-existant class', function(done) {
+    connection.db(testDbName).describe(uuid.v4(), function(err, info) {
+      assert(err instanceof errors.NoClassError);
+      done();
     });
-    it('should be able to describe the OGraphVertex class', function(done) {
-        connection.db(testDbName).describe('OGraphVertex', function(err, info) {
-            assert.ifError(err);
-
-            assert(info, 'No info received on OGraphVertex class');
-            assert.equal(info.name, 'OGraphVertex');
-
-            done();
-        });
-    });
-
-    it('should be able return a no-class error when attempting to describe a non-existant class', function(done) {
-        connection.db(testDbName).describe(uuid.v4(), function(err, info) {
-            assert(err instanceof errors.NoClassError);
-            done();
-        });
-    });
+  });
 });
