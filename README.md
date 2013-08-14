da5f092132625db0a4df6c45d29b8e8df487f0a6
Damon Oehlman <damon.oehlman@gmail.com>
2013-08-14 21:19:29 +1000

    gendocs

--- README.md
+++ README.md
@@ -1,4 +1,65 @@
-# Orienteer
+# orienteer
 
-Orienteer is a node driver for OrientDB.  At this stage Orienteer is a work in progress, and anyone seeking an OrientDB driver for node should look to [node-orientdb](https://github.com/gabipetrovay/node-orientdb) instead.
+Orienteer is a library designed for working with
+[OrientDB](http://orientdb.org).  Currently it operates over the
+[REST API](https://github.com/orientechnologies/orientdb/wiki/OrientDB-REST)
+but support is also planned for the binary driver also.
 
+[
+![unstable]
+(http://hughsk.github.io/stability-badges/dist/unstable.svg)
+](http://github.com/hughsk/stability-badges)
+
+If you are primarily interested in working with the binary driver, however,
+I'd recommend taking a look at
+[node-orientdb](https://github.com/gabipetrovay/node-orientdb) instead.
+
+## Installation
+
+```
+npm install orienteer --save
+```
+
+## Getting Started
+
+To be completed.
+
+```js
+var orienteer = require('orienteer');
+```
+
+## Reference
+
+### orienteer(opts)
+
+Create a new logical OrientDB connection.
+
+### orienteer.rid(value)
+
+Create a new Orienteer RID object that serializes to SQL as expected.
+
+## orienteer http protocol driver
+
+### command(connection, data, callback)
+
+Issue an SQL command against the specified connection.
+
+### describeClass(connection, data, callback)
+
+Describe the specified class.
+
+### describeDb
+
+Issue a connect request against the specified database.
+
+### dbCreate
+
+Create a new database instance.
+
+### dbDelete
+
+Delete the specified db
+
+### dbExist
+
+Check to see if the requested db exists.
--- index.js
+++ index.js
@@ -1,37 +1,78 @@
-var Connection = require('./lib/connection'),
-    OrientError = require('./lib/errors').OrientError,
-    OrientRID = require('./lib/rid'),
-    _ = require('underscore'),
-    converters = require('./lib/converters');
+/* jshint node: true */
+'use strict';
 
+var Connection = require('./lib/connection');
+var OrientError = require('./lib/errors').OrientError;
+var OrientRID = require('./lib/rid');
+var _ = require('underscore');
+var converters = require('./lib/converters');
+
+/**
+  # orienteer
+
+  Orienteer is a library designed for working with
+  [OrientDB](http://orientdb.org).  Currently it operates over the
+  [REST API](https://github.com/orientechnologies/orientdb/wiki/OrientDB-REST)
+  but support is also planned for the binary driver also.
+
+  [
+  ![unstable]
+  (http://hughsk.github.io/stability-badges/dist/unstable.svg)
+  ](http://github.com/hughsk/stability-badges)
+
+  If you are primarily interested in working with the binary driver, however,
+  I'd recommend taking a look at
+  [node-orientdb](https://github.com/gabipetrovay/node-orientdb) instead.
+
+  ## Installation
+
+  ```
+  npm install orienteer --save
+  ```
+
+  ## Getting Started
+
+  To be completed.
+
+  ```js
+  var orienteer = require('orienteer');
+  ```
+
+  ## Reference
+**/
+
+/**
+  ### orienteer(opts)
+
+  Create a new logical OrientDB connection.
+**/
 var orienteer = module.exports = function(opts) {
-    return new Connection(opts);
+  return new Connection(opts);
 };
 
 /**
-## objectTo(targetType, data)
+  ### orienteer.objectTo(targetType, data)
 
-The objectTo function is a reimplementation of the hashToSQLSets function from the
-node-orientdb library.
+  The objectTo function is a reimplementation of the hashToSQLSets function
+  from the node-orientdb library.
 */
-//
 orienteer.objectTo = function(target, data) {
-	var converter = converters['objectTo' + target];
+  var converter = converters['objectTo' + target];
 
-	if (typeof converter != 'function') {
-		throw new Error('Unable to convert object to ' + target);
-	}
+  if (typeof converter != 'function') {
+    throw new Error('Unable to convert object to ' + target);
+  }
 
-	return converter(data);
+  return converter(data);
 };
 
 /**
-## rid(value)
+  ### orienteer.rid(value)
 
-Create a new Orienteer RID object that serializes to SQL as expected.
-*/
+  Create a new Orienteer RID object that serializes to SQL as expected.
+**/
 orienteer.rid = function(value) {
-    return new OrientRID(value);
+  return new OrientRID(value);
 };
 
 // export the error codes
